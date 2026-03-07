b0ed686b205a59769d675a70da1498f1a6276495
Nurfinka <nurfinka.lailasarii@gmail.com>
2026-03-07 22:03:24 +0700

    Implementasi DB Facade

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\SalesController;
+use App\Http\Controllers\LevelController;
 
-Route::get('/', function () {
+Route::get('/', function() {
     return view('welcome');
 });
 
-// Halaman Home
-Route::get('/', [HomeController::class, 'index']);
-
-// Halaman Products
-Route::get('/category/food-beverage', [ProductController::class, 'foodBeverage']);
-Route::get('/category/beauty-health', [ProductController::class, 'beautyHealth']);
-Route::get('/category/home-care', [ProductController::class, 'homeCare']);
-Route::get('/category/baby-kid', [ProductController::class, 'babyKid']);
-
-// Halaman User 
-Route::get('/user/{id}/name/{name}', [UserController::class, 'profile']);
-
-//Halaman Sales
-Route::get('/sales', [SalesController::class, 'index']);
+Route::get('/level', [LevelController::class, 'index']);
