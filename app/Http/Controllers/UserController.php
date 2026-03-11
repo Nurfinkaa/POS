d3f3aec236fc7d2bdc8f1ae4f393c5e1912cbe58
Nurfinka <nurfinka.lailasarii@gmail.com>
2026-03-11 08:49:28 +0700

    Praktikum 2.2 – Not Found Exceptions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\UserModel;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class UserController extends Controller
 {
@@ -20,9 +21,7 @@ class UserController extends Controller
     //UserModel::create($data);
 
     //Ambil semua data dari table m_user
-    $user = UserModel::findOr(20, ['username', 'nama'], function(){
-        abort(404);
-    });
+    $user = UserModel::where('username', 'manager9')->firstOrFail();
     return view('user', ['data' => $user]);
 }
 }
